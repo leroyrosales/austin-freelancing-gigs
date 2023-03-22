24a3564b9870b26b0439bcfbcf9c590f01d1aa36
Leroy Rosales <leroyrosales@gmail.com>
2023-03-22 11:08:49 -0500

    Adds examples of routing

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::get('/posts', function() {
+//     return response()->json(
+//         [
+//             'posts' => [
+//                 [
+//                     'title' => 'hello world post'
+//                 ]
+//             ]
+//         ]
+//     );
+// });
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/hello', function () {
+//     return response('<h1>hello world</h1>', 200)
+//     ->header('Content-Type', 'text/plain');
+// });
+
+// Route::get('/posts/{id}', function($id) {
+//     dd($id);
+//     return response('Post ' . $id);
+// })->where('id', '[0-9]+');
+
+// Route::get('/search', function(Request $request) {
+//    return $request->name;
+// });
