527e80e10ec09e45f45c29cef8ec116129bfe01f
Leroy Rosales <leroyrosales@gmail.com>
2023-03-22 15:59:44 -0500

    Adds listing controller

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Listing;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,19 +17,10 @@ use App\Models\Listing;
 */
 
 // All listings
-Route::get('/', function () {
-    return view('listings', [
-        'heading' => 'Latest gigs',
-        'listings' => Listing::all()
-    ]);
-});
+Route::get('/', [ListingController::class, 'index']);
 
 // Single listings
-Route::get('/listings/{listing}', function(Listing $listing) {
-    return view('listing', [
-        'listing' => $listing
-    ]);
-});
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
 
 // Route::get('/hello', function () {
