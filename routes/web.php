da58dc18e9f7226dd7e44698acc29b62909beef0
Leroy Rosales <leroyrosales@gmail.com>
2023-03-22 14:51:31 -0500

    Makes listing route snappier

--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,9 @@ Route::get('/', function () {
 });
 
 // Single listings
-Route::get('/listings/{id}', function($id) {
+Route::get('/listings/{listing}', function(Listing $listing) {
     return view('listing', [
-        'listing' => Listing::find($id)
+        'listing' => $listing
     ]);
 });
 
