2ac5f5ff64d4d9166ad32b444e410bb76d00a825
Leroy Rosales <leroyrosales@gmail.com>
2023-03-22 15:53:39 -0500

    Adds tags component

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -6,7 +6,7 @@
 <a href="/" class="inline-block text-black ml-4 mb-4"><i class="fa-solid fa-arrow-left"></i> Back
 </a>
 <div class="mx-4">
-    <x-card class="p-24">
+    <x-card>
         <div
             class="flex flex-col items-center justify-center text-center"
         >
@@ -19,28 +19,7 @@
 
             <h3 class="text-2xl mb-2">{{ $listing->title }}</h3>
             <div class="text-xl font-bold mb-4">{{ $listing->company }}</div>
-            <ul class="flex">
-                <li
-                    class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-                >
-                    <a href="#">Laravel</a>
-                </li>
-                <li
-                    class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-                >
-                    <a href="#">API</a>
-                </li>
-                <li
-                    class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-                >
-                    <a href="#">Backend</a>
-                </li>
-                <li
-                    class="bg-black text-white rounded-xl px-3 py-1 mr-2"
-                >
-                    <a href="#">Vue</a>
-                </li>
-            </ul>
+            <x-tags :tagsCsv="$listing->tags"/>
             <div class="text-lg my-4">
                 <i class="fa-solid fa-location-dot"></i> {{ $listing->location }}
             </div>
