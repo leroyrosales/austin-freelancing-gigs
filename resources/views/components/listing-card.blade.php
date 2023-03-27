ee151acf03fe49c446004e0c2baf8504dfca9060
Leroy Rosales <leroyrosales@gmail.com>
2023-03-26 20:34:08 -0500

    Adds illustration to frontpage

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -1,6 +1,6 @@
 @props(['listing'])
 
-    <div class="flex">
+    <article class="flex">
         <img class="hidden w-48 mr-6 md:block" src="{{ ($listing->logo ? asset('storage/' . $listing->logo) : asset('images/no-image.png') ) }}" alt="" role="presentation"/>
         <div>
             <h3 class="text-2xl">
@@ -12,5 +12,5 @@
                 <i class="fa-solid fa-location-dot"></i> {{ $listing->location }}
             </div>
         </div>
-    </div>
+    </article>
 
